4614428755658cfc42c6a758fafb2566a02c3532
kim-soohyeon <kimsh1691@gmail.com>
2021-08-16 17:07:06 +0900

    변수와 자료형 - 진법, 보수

---
+++ src/binary/BinaryTest.java
@@ -0,0 +1,21 @@
+package binary;
+
+public class BinaryTest {
+	
+	//2진수, 8진수, 16진수
+	public static void main(String[] args) {
+
+		int num = 10;
+		//0B: 2진수, 0: 8진수, 0X: 16진수
+		int bNum = 0B1010; //10
+		int oNum = 012;
+		int hNum = 0XA;
+		
+		System.out.println(num);
+		System.out.println(bNum);
+		System.out.println(oNum);
+		System.out.println(hNum);
+		
+	}
+
+}
