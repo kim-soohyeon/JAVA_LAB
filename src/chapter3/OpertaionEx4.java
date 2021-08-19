bc5c1dfa9394e6a168871be6ef4107eeab8dc739
kim-soohyeon <kimsh1691@gmail.com>
2021-08-19 17:31:24 +0900

    복합 대입 연산자

---
+++ src/chapter3/OpertaionEx4.java
@@ -0,0 +1,15 @@
+package chapter3;
+
+public class OpertaionEx4 {
+
+	public static void main(String[] args) {
+		//복합 대입 연산자
+		int num1 = 10;
+		System.out.println(num1 += 1);
+		
+		System.out.println(num1 %= 10);
+		num1 -= 1;
+		System.out.println(num1);
+	}
+
+}
